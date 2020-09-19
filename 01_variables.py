d7c1e12f57a3225ed0344c1d42ca9dcfda0a161b
tai-vu <tai-vu@cybozu.vn>
2020-09-19 16:04:47 +0700

    Variables in python.

---
+++ 01_variables.py
@@ -0,0 +1,12 @@
+# This is a comment
+
+'''
+This is a paragraph.
+Because I want to comment a lot of content.
+'''
+
+name = "Vcttai"
+print(f"Hello world! My name is {name}.")
+
+appleColor, lemonColor = "red", "yellow"
+print(f"Apple is {appleColor} and Lemon is {lemonColor}.")
